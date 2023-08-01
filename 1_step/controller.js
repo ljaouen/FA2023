45c7e59dd317ca5f02dc732dd65142661ed49cd4
Luc <luc.jaouen@matelys.com>
2023-08-01 08:45:48 +0200

    code release

---
+++ 1_step/controller.js
@@ -0,0 +1,93 @@
+"use strict"; // special Javascript directive to avoid trouble with undefined variables
+
+const P0 = 2e-5; // Reference pressure in Pascal
+
+let soundPressureLevels = [50, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // Sound pressure levels of 10 uncorrelated sound source in deciBel
+let L1, L2, L3, L4, L5, L6, L7, L8, L9; // Input elements for sound pressure level of each uncorrelated sound source
+let result_div;
+let result = 0; 
+
+
+// Convert pressure from Pascal to deciBel
+function convertPressureFromPaTodB(pressure) {
+    return 20 * Math.log10(pressure / P0);
+}
+
+// Convert pressure from deciBel to Pascal
+function convertPressureFromDBToPa(pressure) {
+    return Math.pow(10, pressure / 20) * P0;
+}
+
+// Compute the global sound pressure level from the sound pressure level of each uncorrelated sound source
+function computeGlobalSoundPressureLevel(soundPressureLevels) {
+    return 10 * Math.log10(
+        soundPressureLevels.reduce((sum, soundPressureLevel) => {
+            return sum + Math.pow(10, soundPressureLevel / 10);
+        }, 0)
+    );
+}
+
+// When the page and all related files are loaded, call the init function and start listening
+// to the input text boxes for changes
+document.addEventListener("DOMContentLoaded", init, false);
+
+function init() {
+
+    // Initialize the Numscrubber library
+    Numscrubber.init();
+
+    result_div = document.getElementById("result");
+
+    document.getElementById("L0").value = 50;
+    document.getElementById("L0").style.background = "linear-gradient(to right,#ccc 0%,#ccc 50%,#fff 50%,#fff 100%)";
+
+    document.getElementById("L1").value = 0;
+    document.getElementById("L1").style.background = "#fff";
+
+    document.getElementById("L2").value = 0;
+    document.getElementById("L2").style.background = "#fff";
+
+    document.getElementById("L3").value = 0;
+    document.getElementById("L3").style.background = "#fff";
+
+    document.getElementById("L4").value = 0;
+    document.getElementById("L4").style.background = "#fff";
+
+    document.getElementById("L5").value = 0;
+    document.getElementById("L5").style.background = "#fff";
+
+    document.getElementById("L6").value = 0;
+    document.getElementById("L6").style.background = "#fff";
+
+    document.getElementById("L7").value = 0;
+    document.getElementById("L7").style.background = "#fff";
+
+    document.getElementById("L8").value = 0;
+    document.getElementById("L8").style.background = "#fff";
+
+    document.getElementById("L9").value = 0;
+    document.getElementById("L9").style.background = "#fff";
+
+    // Catch any change in the input boxes and update their 2-color backgrounds to reflect their values
+    for (const i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        document.getElementById('L' + i).oninput = function() {
+            this.style.background = "linear-gradient(to right,#ccc 0%,#ccc " + parseInt(this.value) + "%,#fff " + parseInt(this.value) + "%,#fff 100%)";
+            soundPressureLevels[i] = parseInt(this.value);
+            updateResult();
+        }
+    }
+
+    // The default value for the global sound pressure level is 50 dB (one source at 50 dB)
+    result_div.innerHTML = "50";
+}
+
+// Convert integer from 60 to 100 to a percentage scale
+function convertIntToPercentage(int) {
+    return (int - 60) / 40 * 100;
+}
+
+// Modify the result displayed on the page
+function updateResult() {
+    result = computeGlobalSoundPressureLevel(soundPressureLevels).toFixed(0);
+    result_div.innerHTML = result;
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This repository contains the HTML, CSS and Javascript of the presentation done during :
 Forum Acusticum 2023 (Sept. 11-15 2023, Torino, Italy)
 with
-Title : Live coding of a web interactive application
+Title : Live coding of an nteractive web application
 Author : Luc Jaouen (Matelys)
 in session : A22-02, Interactive media or demonstrations for lectures and laboratory
 
@@ -10,7 +10,12 @@ During this presentation, I propose to show how to code, from scratch, a simple 
 Examples of such webpages are discussed in L. Jaouen and O. Robin, "Explaining and teaching acoustics through comics, interactive web pages, and video games", J. Acoust. Soc. Am. 152 (2), pp. 745-752, 2022, https://doi.org/10.1121/10.0013011
 and are available at: https://apmr.matelys.com/BasicsAcoustics/1A_EN/
 
-For this presentation, a basic HTML + responsive CSS template will be used together with Bret Victor's "Tangle" Javascript library for interactivity (see http://worrydream.com/Tangle/guide.html). This template can be used to easily create new interactive pages.
+For this presentation, a basic HTML will be used. 
+This template can be used to easily create new interactive pages.
  
 The full code for this interactive web application is available at https://github.com/ljaouen/FA2023 
+
+# License
+license Attribution 3.0 Unported (CC BY 3.0)
+see https://creativecommons.org/licenses/by/3.0/
 
